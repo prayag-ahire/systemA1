b10512c7a0a77791c26b4a8ef9ef2b6a5c2fd89f
Ahire Prayag <prayagahire365@gmail.com>
2026-02-22 23:04:15 +0530

    execution function and basic worker funciton is done

--- src/worker.ts
+++ src/worker.ts
@@ -1,22 +1,24 @@
+import prisma from "./lib/prisma";
+
 let isShuttingDown = false;
 
 async function executeTask() {
     await new Promise((resolve) => {
-        const num = Math.random() * 1000
+        const num = Math.random() * 10000
         if (num > 8000) {
             throw new Error("task fail")
         }
-        setTimeout(resolve, 1000)
+        setTimeout(resolve, 1000);
     })
 }
 
 async function worker() {
     while (!isShuttingDown) {
         try {
-            const task = await prisma?.$queryRawUnsafe<any[]>(`
+            const task1 = await prisma.$queryRawUnsafe<any[]>(`
                 update  "Task"
                 set status = 'RUNNING',
-                    "startedAt" = new()
+                    "startedAt" = now()
                 where id =(
                     select t.id from "Task" t
                     where t.status = 'QUEUED'
@@ -25,14 +27,35 @@ async function worker() {
                         select 1 from "TaskDependency" td
                         join "Task" dep on td."dependsOnId" = dep.id
                         where td."taskId" = t.id
-                        and dep.status != 'SUCCESS'
+                        and dep.status != 'COMPLETED'
                     )
                     order by t."runAt" ASC, t."createdAt" ASC
                     for update skip locked
                     limit 1
                 )
                 returning *;
-                `)
+                `);
+
+            const task = task1[0];
+
+            if (!task) {
+                await new Promise((resolve) => {
+                    setTimeout(resolve, 2500);
+                })
+                continue;
+            }
+            try {
+                await executeTask();
+                await prisma.task.update({
+                    where: { id: task.id },
+                    data: { status: "COMPLETED", startedAt: null },
+                });
+            } catch (error) {
+                await prisma.task.update({
+                    where: { id: task.id },
+                    data: { status: "FAILED", startedAt: null },
+                });
+            }
         } catch {
             await new Promise((resolve) => setTimeout(resolve, 5000));
         }
