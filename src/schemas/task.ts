31950a86a6e880d15bd39add031173790d11bab3
Ahire Prayag <prayagahire365@gmail.com>
2026-02-22 21:39:31 +0530

    created post task and make changes in prisma task schema

--- src/schemas/task.ts
+++ src/schemas/task.ts
@@ -5,7 +5,7 @@ export const TaskSchema = z.object({
     type: z.string().min(1),
     payload: z.record(z.string(), z.any()),
     runAt: z.string().datetime().optional(),
-    maxAttempt: z.number().int().positive().optional(),
+    maxAttempts: z.number().int().positive().optional(),
     dependsOn: z.array(z.string().uuid()).optional()
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import rateLimit from "express-rate-limit";
 import cors from "cors";
+import { TaskSchema } from "./schemas/task";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +16,49 @@ app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/task", async (req, res) => {
+    try {
 
+        const tasks = await prisma?.task.findMany({
+            orderBy: { createdAt: "desc" },
+        });
+        return res.json(tasks);
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error" })
+    }
+})
+
+app.post("/task", async (req, res) => {
+    try {
+        const validated = TaskSchema.safeParse(req.body);
+
+        if (!validated.success) {
+            return res.status(400).json({
+                error: "Invalid input data",
+                details: validated.error.flatten(),
+            });
+        }
+        const { id, type, payload, runAt, maxAttempts, dependsOn } = validated.data;
+
+        const task = await prisma.task.create({
+            data: {
+                id,
+                type,
+                payload,
+                runAt: runAt ? new Date(runAt) : new Date(),
+                maxAttempts: maxAttempts ?? 3,
+                status: "QUEUED",
+                dependents: dependsOn ? {
+                    create: dependsOn.map(depId => ({
+                        dependsOnId: depId
+                    }))
+                } : undefined
+            },
+        });
+        return res.status(201).json(task);
+
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error" })
+    }
 })
 
 
